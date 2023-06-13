d468f35d6089591b1af2254871ec1fdb6f22bc67
williamxmejia <willx1995@gmail.com>
2023-06-13 12:05:27 -0500

    feat: dynamically set the days

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,69 +1,40 @@
-import React from "react";
+import React from "react"
+import dayjs from "dayjs"
+import weekdayPlugin from "dayjs/plugin/weekday"
+import isTodayPlugin from "dayjs/plugin/isToday"
+import objectPlugin from "dayjs/plugin/toObject"
 
 function Calendar() {
-  const days = [
-    "sun",
-    "mon",
-    "tue",
-    "wed",
-    "thu",
-    "fri",
-    "sat",
-  ];
+  dayjs.extend(weekdayPlugin)
+  dayjs.extend(isTodayPlugin)
+  dayjs.extend(objectPlugin)
 
-  let i = 1
+  const currDate = dayjs()
+  
 
+  function renderDays() {
+    const dateFormat = "ddd"
+    const days = []
 
+    for (let i = 0; i < 7; i++) {
+      days.push(
+          <div className="calendar-days" key={i}>{currDate.weekday(i).format(dateFormat)}</div>
+      )
+    }
+
+    return days
+  }
+  
   return (
     <>
       <div className="cont-box">
         <h1>June</h1>
         <div className="calendar-head">
-          {days.map((day) => (
-            <div>
-              <div className="calendar-days">{day}</div>
-              {/* <div className="calendar-in">{i++}</div> */}
-            </div>
-
-          ))}
-          {/* <div className="calendar-in">2</div> */}
-
-        </div>
-        <div className="calendar-head">
-          <div className="calendar-in">1</div>
-          <div className="calendar-in">2</div>
-          <div className="calendar-in">3</div>
-          <div className="calendar-in">4</div>
-          <div className="calendar-in">5</div>
-          <div className="calendar-in">6</div>
-          <div className="calendar-in">7</div>
-          <div className="calendar-in">8</div>
-          <div className="calendar-in">9</div>
-          <div className="calendar-in">10</div>
-          <div className="calendar-in">11</div>
-          <div className="calendar-in">12</div>
-          <div className="calendar-in">13</div>
-          <div className="calendar-in">14</div>
-          <div className="calendar-in">15</div>
-          <div className="calendar-in">16</div>
-          <div className="calendar-in">17</div>
-          <div className="calendar-in">18</div>
-          <div className="calendar-in">19</div>
-          <div className="calendar-in">20</div>
-          <div className="calendar-in">21</div>
-          <div className="calendar-in">22</div>
-          <div className="calendar-in">23</div>
-          <div className="calendar-in">24</div>
-          <div className="calendar-in">25</div>
-          <div className="calendar-in">26</div>
-          <div className="calendar-in">27</div>
-          <div className="calendar-in">28</div>
-          <div className="calendar-in">29</div>
-          <div className="calendar-in">30</div>
+          {renderDays()}
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default Calendar;
+export default Calendar
