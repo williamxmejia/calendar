8f0024ed47262bdc129cef0d25725b91d68ca62e
williamxmejia <willx1995@gmail.com>
2023-06-16 00:36:11 -0500

    feat: back and next buttons work for the months

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,16 +3,18 @@ import dayjs from "dayjs"
 import weekdayPlugin from "dayjs/plugin/weekday"
 import isTodayPlugin from "dayjs/plugin/isToday"
 import objectPlugin from "dayjs/plugin/toObject"
+import localeData from "dayjs/plugin/localeData"
+
+dayjs.extend(weekdayPlugin)
+dayjs.extend(isTodayPlugin)
+dayjs.extend(objectPlugin)
+dayjs.extend(localeData)
 
 function Calendar() {
-  dayjs.extend(weekdayPlugin)
-  dayjs.extend(isTodayPlugin)
-  dayjs.extend(objectPlugin)
   const currDate = dayjs()
-
-  const [currentMonth, setCurrentMonth] =  useState(currDate)
+  const months = dayjs.months()
+  const [currentMonth, setCurrentMonth] = useState(currDate)
   const [dates, setArrayOfDates] = useState([])
-  
 
   function renderDays() {
     const dateFormat = "ddd"
@@ -20,35 +22,33 @@ function Calendar() {
 
     for (let i = 0; i < 7; i++) {
       days.push(
-          <div className="calendar-days" key={i}>{currDate.weekday(i).format(dateFormat)}</div>
-      )
+        <div className="calendar-days" key={i}>
+          {currDate.weekday(i).format(dateFormat)}
+        </div>
+      );
     }
-
     return days
   }
 
-  function getAllDays () {
-    let currentDate = currentMonth.startOf('month').weekday(0)
-    const nextMonth = currentMonth.add(1, 'month')
-    const nextMonthEnd = currentMonth.add(1, 'month').month()
-    // const startDate = dayjs(`2023-07-01`);
-    const endDate = nextMonth.startOf('month').weekday(6);
+  function getAllDays() {
+    let currentDate = currentMonth.startOf("month").weekday(0)
+    const nextMonth = currentMonth.add(1, "month")
+    const nextMonthEnd = currentMonth.add(1, "month").month()
+    // const startDate = dayjs(`2023-07-01`)
+    const endDate = nextMonth.startOf("month").weekday(6)
 
     let allDates = []
     let weekDates = []
 
     let weekCounter = 1
     let dayCounter = 0
-    let b = currentMonth.startOf('month').weekday(0)
+    let b = currentMonth.startOf("month").weekday(0)
 
-
-    while(b.isBefore(endDate) || b.isSame(endDate, 'day')) {
-
+    while (b.isBefore(endDate) || b.isSame(endDate, "day")) {
       const formated = formatDateObject(b)
-
       weekDates.push(formated)
 
-      if(weekCounter === 7) {
+      if (weekCounter === 7) {
         allDates.push(weekDates)
         weekDates = []
         weekCounter = 0
@@ -56,82 +56,77 @@ function Calendar() {
 
       weekCounter++
       dayCounter++
-      b = currentDate.add(dayCounter, 'day')
-
-
+      b = currentDate.add(dayCounter, "day")
     }
     setArrayOfDates(allDates)
-    
   }
 
-
-  console.log(dates)
-
-
-  useEffect (() => {
+  useEffect(() => {
     getAllDays()
   }, [currentMonth])
 
-  function renderCells () {
+  function renderCells() {
     const rows = []
     let days = []
 
-    // dates.forEach((week, index) => {
-    //   week.forEach((d, i) => {
-    //     days.push (
-    //       <div>{d.day}</div>
-    //     )
-    //   })
-    //   rows.push({days})
-    //   days = []
-    // })
-
-    dates.map(week => {
-      week.map(d => {
-        days.push(
-          <div className="calendar-in">{d.day}</div>
-        )
+    dates.map((week) => {
+      week.map((d) => {
+        days.push(<div className="calendar-in">{d.day}</div>)
       })
-      rows.push(
-        days
-      )
+      rows.push(days)
       days = []
     })
 
-    console.log(rows)
-
     return rows
-    
   }
 
+  function renderButtons() {
 
+    return (
+      <div className="calendar-head">
+      <button onClick={monthHandlerBack} type="button">
+        {"<"}
+      </button>
+      <button onClick={monthHandlerForward} type="button">
+        {">"}
+      </button>
+    </div>
+    )
+  }
 
+  function monthHandlerBack() {
+    let back = currentMonth.subtract(1, "month")
+    setCurrentMonth(back)
+    console.log(back)
+  }
 
-  function formatDateObject (date) {
-    const clonedObject = {...date.toObject()}
+  function monthHandlerForward() {
+    let next = currentMonth.add(1, "month")
+    setCurrentMonth(next)
+    console.log(next)
+  }
+
+  function formatDateObject(date) {
+    const clonedObject = { ...date.toObject() }
 
     const formatObject = {
       day: clonedObject.date,
       month: clonedObject.months,
       year: clonedObject.years,
-      isCurrentDay: date.isToday()
+      isCurrentDay: date.isToday(),
     }
 
     return formatObject
   }
 
-
-
-
-  
   return (
     <>
       <div className="cont-box">
-        <h1>June</h1>
+        <h1>{months[currentMonth.month()]}</h1>
+        {renderButtons()}
         <div className="calendar-head">
           {renderDays()}
           {renderCells()}
-          {/* {getAllDays()} */}
         </div>
       </div>
     </>
